9cdda578c9fe7d7837be4d3a932efc490f0dd3f7
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-06-13 06:27:38 +0200

    First Commit Initial User&Admin Layouts

---
+++ Charity-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Charity_Website.Startup))]
+namespace Charity_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
